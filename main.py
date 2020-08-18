d236324ff4f559e389a7e27cd38f0b915f9c0ee3
Gavin Hunter <clghunter2@glow.sch.uk>
2020-08-18 09:34:37 +0000

    template

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+Turtle Graphics
+
+This is a picture of *****, created with Python Turtle
+
+https://docs.python.org/3/library/turtle.html
+'''
+
+# load the turtle module so that we can use it
+import turtle
+
+# create a turtle object called timmy
+# this is a copy of the turtle module with all the data and functions we need
+timmy = turtle.Turtle()
+
+# run the forward function for timmy (send the lenght as an argument)
+timmy.forward(100)
+
